50434910840373d60870184bf481f46826f5ce94
Wahid Tahosin Laam <laamwahidtahosin@gmail.com>
2025-10-21 09:05:54 +0600

    nav done

---
+++ src/Components/Pages/Home/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { IoIosArrowDown } from 'react-icons/io'
+import PrimaryBtn from '../../Utility/PrimaryBtn'
+
+const Navbar = () => {
+  return (
+    <nav className='container pt-2.5 bg-primary'>
+      <div className='flex justify-between items-center'>
+
+      
+      <div>
+        <a href="">
+        <img src="images/Logo.png" alt="Logo" />
+        </a>
+      </div>
+      <div>
+        <ul className='flex flex-row gap-10 text-white font-semibold'>
+          <li>
+            <a href="#" className="inline-flex items-center gap-1">
+              <span>Home</span>
+              <IoIosArrowDown aria-hidden="true" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="inline-flex items-center gap-1">
+              <span>About</span>
+              <IoIosArrowDown aria-hidden="true" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="inline-flex items-center gap-1">
+              <span>Service</span>
+              <IoIosArrowDown aria-hidden="true" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="inline-flex items-center gap-1">
+              <span>Portfolio</span>
+              <IoIosArrowDown aria-hidden="true" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="inline-flex items-center gap-1">
+              <span>Price</span>
+              <IoIosArrowDown aria-hidden="true" />
+            </a>
+          </li>
+          <li>
+            <a href="#" className="inline-flex items-center gap-1">
+              <span>Blog</span>
+              <IoIosArrowDown aria-hidden="true" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <PrimaryBtn/>
+      </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/Components/Utility/PrimaryBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PrimaryBtn = () => {
+  return (
+    <div className='bg-secondary py-3.5 px-7'>
+      <button className='text-white font-semibold text-lg'>
+        Contact Us
+      </button>
+    </div>
+  )
+}
+
+export default PrimaryBtn
